ce041bc8c82f8a97dd3c04991ff74993f724486a
kassym88 <kassym88@gmail.com>
2018-11-01 17:38:04 +0600

    binding input

--- src/js/components/layout/Counter.jsx
+++ src/js/components/layout/Counter.jsx
@@ -3,10 +3,20 @@ import {connect} from 'react-redux';
 import {mapStateToProps} from '../../redux/reducers';
 
 class Counter extends React.Component {
-    constructor(props){
-        super(props);
-        // console.log('constructor Counter', this.props);
-    }
+    // constructor(props){
+    //     super(props);
+    //     // console.log('constructor Counter', this.props);
+    //     this.state = {
+    //         val1: 'val1'
+    //     };
+    //     // this.val2 = React.createRef();
+    // }
+
+    state = {
+        val1: 'val1'
+    };
+
+    val2 = React.createRef();
 
     componentDidUpdate(prevProps){
         // console.log('componentDidUpdate Counter', this.props);
@@ -20,6 +30,15 @@ class Counter extends React.Component {
         this.props.dispatch({type: 'DECREMENT'});
     };
 
+    handleChange = (event) => {
+        this.setState({val1: event.target.value});
+    };
+
+    pushMe = () => {
+        console.log('input val1', this.state.val1);
+        console.log('input val2', this.val2.current.value);
+    };
+
     render() {
         return (
             <div>
@@ -28,6 +47,11 @@ class Counter extends React.Component {
                     <button className="btn-sm btn-primary" onClick={this.decrement}>-</button>
                     <span style={{margin: '10px'}}>{this.props.count}</span>
                     <button className="btn-sm btn-danger" onClick={this.increment}>+</button>
+                </div>
+                <div>
+                    <input type="text" value={this.state.val1} onChange={this.handleChange}/>
+                    <input type="text" defaultValue="val2" ref={this.val2}/>
+                    <button className="btn btn-secondary" onClick={this.pushMe}>PushMe</button>
                 </div>
             </div>
         )
